1ed8e8e8577117ac41b19790b59e4a0fddd3661f
richardboyz <r1382333@gmail.com>
2023-10-17 00:40:52 +0800

    Add signout method in AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,17 @@
-import { createContext, useContext } from "react";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
 
 import { auth } from "../firebase";
 
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
   const updateUserDisplayName = (authInstence, displayName) => {
     return updateProfile(authInstence.currentUser, {
       displayName: displayName,
@@ -20,8 +26,22 @@ export const AuthContextProvider = ({ children }) => {
     );
   };
 
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
-    <UserContext.Provider value={{ createUser }}>
+    <UserContext.Provider value={{ createUser, user, logout }}>
       {children}
     </UserContext.Provider>
   );
