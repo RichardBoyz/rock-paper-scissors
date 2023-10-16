7830e4d930375f5e2c6bb90def6d7535a3a2bba9
richardboyz <r1382333@gmail.com>
2023-10-17 00:28:47 +0800

    Add Home component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
+import Home from "./components/Home";
 import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/Home" element={<Home />} />
         </Routes>
       </AuthContextProvider>
     </div>
