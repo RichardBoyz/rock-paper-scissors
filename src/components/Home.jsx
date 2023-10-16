3a5db216ac057838c63d29625326a6ff43d2671d
richardboyz <r1382333@gmail.com>
2023-10-17 00:41:09 +0800

    Set dummy Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-
+import { UserAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
-  return <div className="home">Home</div>;
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+      console.log("已登出");
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+  return (
+    <div className="home">
+      <p>使用者: {user && user.displayName}</p>
+      <button onClick={handleLogout}>登出</button>
+    </div>
+  );
 };
 
 export default Home;
