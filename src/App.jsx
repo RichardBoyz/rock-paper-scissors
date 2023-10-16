d503af9de468ceb1cca4a5073e2efba819f91b98
richardboyz <r1382333@gmail.com>
2023-10-17 00:26:16 +0800

    Set AuthContext to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <div className="app">
       <h1 className="app__title">剪刀 石頭 布</h1>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
