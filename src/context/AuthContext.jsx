61eae16951f4821bf903cd97fb662f1fdb206ad7
richardboyz <r1382333@gmail.com>
2023-10-17 00:25:54 +0800

    Set createUser method in provider

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,30 @@
 import { createContext, useContext } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+import { auth } from "../firebase";
 
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  return <UserContext.Provider>{children}</UserContext.Provider>;
+  const updateUserDisplayName = (authInstence, displayName) => {
+    return updateProfile(authInstence.currentUser, {
+      displayName: displayName,
+    });
+  };
+
+  const createUser = async (email, password, displayName) => {
+    await createUserWithEmailAndPassword(auth, email, password).then(
+      (userCredential) => {
+        updateUserDisplayName(userCredential.user.auth, displayName);
+      }
+    );
+  };
+
+  return (
+    <UserContext.Provider value={{ createUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const UserAuth = () => {
