e5896cca4e41288914933781161ed98be0339470
richardboyz <r1382333@gmail.com>
2023-10-16 23:54:34 +0800

    Set apps route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <h1 className="app__title">剪刀 石頭 布</h1>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
