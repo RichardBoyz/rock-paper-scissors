90d83777e1c5531bde2540ae0c46c49594b968e7
richardboyz <r1382333@gmail.com>
2023-10-17 22:03:34 +0800

    Set Home layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { UserAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import "./Home.scss";
 const Home = () => {
   const { user, logout } = UserAuth();
   const navigate = useNavigate();
@@ -12,10 +13,17 @@ const Home = () => {
       console.log(e.message);
     }
   };
+
   return (
     <div className="home">
-      <p>使用者: {user && user.displayName}</p>
       <button onClick={handleLogout}>登出</button>
+
+      <section className="home__container">
+        <div className="home__left">
+          <p>使用者: {user && user.displayName}</p>
+        </div>
+        <div className="home__right">{/* Battle list */}</div>
+      </section>
     </div>
   );
 };
