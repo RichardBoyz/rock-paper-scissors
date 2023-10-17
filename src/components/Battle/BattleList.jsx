cdb076a4f7853f62f37177c7b3308648abe45304
richardboyz <r1382333@gmail.com>
2023-10-18 01:15:35 +0800

    Set BattleCard

--- src/components/Battle/BattleList.jsx
+++ src/components/Battle/BattleList.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./BattleList.scss";
+import BattleCard from "./BattleCard";
+import { doc, collection } from "firebase/firestore";
+import { BattleField } from "../../context/BattleContext";
 
 const BattleList = () => {
-  return <div className="battle-list">BattleList</div>;
+  return (
+    <div className="battle-list">
+      <BattleCard></BattleCard>
+    </div>
+  );
 };
 
 export default BattleList;
