852cd394557499c5ee50048b531dcdc78012f585
richardboyz <r1382333@gmail.com>
2023-10-18 01:12:32 +0800

    Add invite component

---
+++ src/components/Invite/Invite.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "./Invite.scss";
+
+const Invite = () => {
+  const [inviteUsers, setInviteUsers] = useState([]);
+  return (
+    <div className="invite">
+      <section className="invite__search">
+        <input type="text" placeholder="輸入使用者信箱" />
+        <button className="invite__search-button">添加使用者</button>
+      </section>
+
+      <section className="invite__list">{/* Invited user list */}</section>
+
+      <button>建立對戰</button>
+    </div>
+  );
+};
+
+export default Invite;
