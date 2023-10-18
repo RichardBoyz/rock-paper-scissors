227552fd4888edf55e30b8c2ef565adfa38ec1c2
richardboyz <r1382333@gmail.com>
2023-10-18 22:12:50 +0800

    Add invite user to Invited-List

--- src/components/Invite/Invite.jsx
+++ src/components/Invite/Invite.jsx
@@ -1,9 +1,41 @@
 import React, { useState } from "react";
 import "./Invite.scss";
+import { collection, getDocs, where, query } from "@firebase/firestore";
+import { db } from "../../firebase";
+import { UserAuth } from "../../context/AuthContext";
+import Tag from "../common/Tag/Tag";
 
 const Invite = () => {
   const [inviteUsers, setInviteUsers] = useState([]);
   const [userEmail, setUserEmail] = useState("");
+  const { user } = UserAuth();
+
+  const [invalidMessage, setInvalidMessage] = useState("");
+
+  const handleSearchUser = async () => {
+    if (!userEmail) return;
+    if (user.auth.currentUser.email === userEmail) {
+      setInvalidMessage("你已經是內定人員");
+      console.log("已經是內定人員");
+      return;
+    }
+    const usersRef = collection(db, "users");
+    const userQuery = query(usersRef, where("email", "==", userEmail));
+    const docSnap = await getDocs(userQuery);
+    if (docSnap.size > 0) {
+      const userDoc = docSnap.docs[0];
+      console.log(userDoc.id, " => ", userDoc.data());
+      const { displayName, email, uid } = userDoc.data();
+      setInviteUsers((pre) => {
+        return [...inviteUsers, { displayName, email, uid }];
+      });
+    } else {
+      setInvalidMessage("找不到此使用者");
+      console.log("找不到此使用者");
+    }
+  };
+
+  const removeUserFromInvitedList = (userId) => {};
   return (
     <div className="invite">
       <section className="invite__search">
@@ -13,10 +45,20 @@ const Invite = () => {
           value={userEmail}
           onChange={(e) => setUserEmail(e.target.value)}
         />
-        <button className="invite__search-button">添加使用者</button>
+        <button className="invite__search-button" onClick={handleSearchUser}>
+          添加使用者
+        </button>
       </section>
 
-      <section className="invite__list">{/* Invited user list */}</section>
+      <section className="invite__list">
+        {inviteUsers.map((userInfo) => (
+          <Tag
+            key={userInfo.uid}
+            label={userInfo.displayName}
+            dismissTag={() => removeUserFromInvitedList(userInfo.uid)}
+          />
+        ))}
+      </section>
 
       <button>建立對戰</button>
     </div>
