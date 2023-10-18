d832beba880971086a7a0186e09d0f4601619691
richardboyz <r1382333@gmail.com>
2023-10-19 00:45:48 +0800

    Add get battles method

--- src/components/Battle/BattleList.jsx
+++ src/components/Battle/BattleList.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import "./BattleList.scss";
 import BattleCard from "./BattleCard";
-import { doc, collection } from "firebase/firestore";
-import { BattleField } from "../../context/BattleContext";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+import { UserAuth } from "../../context/AuthContext";
 
 const BattleList = () => {
-  return (
-    <div className="battle-list">
-      <BattleCard></BattleCard>
-    </div>
-  );
+  const { user } = UserAuth();
+  const [battles, setBattles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getBattles = async () => {
+    if (!user.auth) return;
+    const battleQuery = query(
+      collection(db, "battles"),
+      where("userIds", "array-contains", user.auth.currentUser.uid)
+    );
+    try {
+      const querySnapshot = await getDocs(battleQuery);
+      const newBattles = [];
+      querySnapshot.forEach((doc) => {
+        newBattles.push({ uid: doc.id, ...doc.data() });
+      });
+      setBattles((pre) => [...newBattles]);
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getBattles();
+  }, [user.auth]);
 };
 
 export default BattleList;
