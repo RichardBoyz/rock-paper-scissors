a5d0b667a0b594cb8064c9cb0c2df420e7ccb754
richardboyz <r1382333@gmail.com>
2023-10-19 00:24:54 +0800

    Complete BattleCard layout

--- src/components/Battle/BattleCard.jsx
+++ src/components/Battle/BattleCard.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./BattleCard.scss";
 
-const BattleCard = () => {
-  return <div className="battle-card">BattleCard</div>;
+const BattleCard = ({ name, uid, isFinished }) => {
+  return (
+    <div className="battle-card">
+      <span>{name}</span>
+      <span style={{ color: isFinished ? "red" : "blue" }}>
+        {isFinished ? "已結束" : "未結束"}
+      </span>
+    </div>
+  );
 };
 
 export default BattleCard;
