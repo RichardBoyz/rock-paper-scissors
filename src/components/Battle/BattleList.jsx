9f2c8ba37c6fd03c925d32ee2c28291c661dd341
richardboyz <r1382333@gmail.com>
2023-10-19 01:29:11 +0800

    Add listen data change in BattleList

--- src/components/Battle/BattleList.jsx
+++ src/components/Battle/BattleList.jsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./BattleList.scss";
 import BattleCard from "./BattleCard";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 import { db } from "../../firebase";
 import { UserAuth } from "../../context/AuthContext";
 
@@ -9,6 +15,37 @@ const BattleList = () => {
   const { user } = UserAuth();
   const [battles, setBattles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [canListen, setCanListen] = useState(false);
+  const findBattles = () => {
+    if (!user.auth) return;
+    setIsLoading(true);
+    const battleQuery = query(
+      collection(db, "battles"),
+      where("userIds", "array-contains", user.auth.currentUser.uid)
+    );
+    const unsub = onSnapshot(battleQuery, (snapShot) => {
+      if (snapShot.docChanges().length) {
+        const battleDataDict = {};
+        battles.forEach((battle, index) => {
+          battleDataDict[battle.uid] = index;
+        });
+        const newBattles = [...battles];
+        console.log(newBattles);
+        snapShot.docChanges().forEach((change) => {
+          if (battleDataDict[change.doc.id] != undefined) {
+            newBattles[battleDataDict[change.doc.id]] = {
+              ...newBattles[battleDataDict[change.doc.id]],
+              ...change.doc.data(),
+            };
+          } else {
+            newBattles.push({ uid: change.doc.id, ...change.doc.data() });
+          }
+        });
+        setBattles((pre) => [...newBattles]);
+      }
+      setIsLoading(false);
+    });
+  };
 
   const getBattles = async () => {
     if (!user.auth) return;
@@ -23,6 +60,7 @@ const BattleList = () => {
         newBattles.push({ uid: doc.id, ...doc.data() });
       });
       setBattles((pre) => [...newBattles]);
+      setCanListen(true);
     } catch (error) {
       console.log(error.message);
     }
@@ -31,13 +69,19 @@ const BattleList = () => {
   };
 
   useEffect(() => {
+    findBattles();
+    return () => {
+      findBattles();
+    };
+  }, [canListen]);
+
+  useEffect(() => {
     getBattles();
   }, [user.auth]);
 
   const buildBattleCard = () => {
     if (isLoading) return <p>Loading...</p>;
     if (!battles.length) return <p>沒有對戰</p>;
-    console.log(battles);
     return battles.map((battle) => (
       <BattleCard
         key={battle.uid}
