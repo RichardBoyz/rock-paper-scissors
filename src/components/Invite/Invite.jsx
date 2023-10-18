a0fcb8ba33681703ead51d465135eea3daa76e51
richardboyz <r1382333@gmail.com>
2023-10-18 22:47:01 +0800

    Add create battle method

--- src/components/Invite/Invite.jsx
+++ src/components/Invite/Invite.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./Invite.scss";
-import { collection, getDocs, where, query } from "@firebase/firestore";
+import { collection, getDocs, where, query, addDoc } from "@firebase/firestore";
 import { db } from "../../firebase";
 import { UserAuth } from "../../context/AuthContext";
 import Tag from "../common/Tag/Tag";
 
 const Invite = () => {
+  const [battleName, setBattleName] = useState("");
   const [inviteUsers, setInviteUsers] = useState([]);
   const [userEmail, setUserEmail] = useState("");
   const { user } = UserAuth();
@@ -42,6 +43,32 @@ const Invite = () => {
     setInviteUsers((pre) => [...newInviteUserList]);
   };
 
+  const createBattle = async () => {
+    const battleUserIds = inviteUsers.map((userInfo) => userInfo.uid);
+    battleUserIds.push(user.auth.currentUser.uid);
+
+    const defaultRound = {
+      isFinshed: false,
+    };
+    const usersResultDict = {};
+    battleUserIds.forEach((id) => {
+      usersResultDict[id] = 0;
+    });
+    defaultRound.usersResult = usersResultDict;
+
+    try {
+      await addDoc(collection(db, "battles"), {
+        name: battleName,
+        userIds: battleUserIds,
+        round: [defaultRound],
+        winner: [],
+        isFinished: false,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="invite">
       <section className="invite__search">
@@ -66,7 +93,18 @@ const Invite = () => {
         ))}
       </section>
 
-      <button disabled={!inviteUsers.length}>建立對戰</button>
+      <input
+        type="text"
+        value={battleName}
+        onChange={(e) => setBattleName(e.target.value)}
+        placeholder="請輸入對戰名稱"
+      />
+      <button
+        disabled={!inviteUsers.length || !battleName.trim()}
+        onClick={createBattle}
+      >
+        建立對戰
+      </button>
     </div>
   );
 };
