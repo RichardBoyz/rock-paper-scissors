41fe154de22a559ebe04d895e86e023ad9471b12
richardboyz <r1382333@gmail.com>
2023-10-20 00:57:17 +0800

    Adjust BattleMessage layout

--- src/components/Battle/BattleMessage.jsx
+++ src/components/Battle/BattleMessage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./BattleMessage.scss";
 
 const BattleMessage = ({ round, finishedUsers, unfinishedUser, result }) => {
   return (
     <div className="battle-message">
       <span>{`第 ${round} 輪`}</span>
-      <span>{result ? "" : "未結算"}</span>
+      <span>{result ?? "未結算"}</span>
     </div>
   );
 };
