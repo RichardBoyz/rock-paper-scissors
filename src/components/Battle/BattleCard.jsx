7153e36eb7fc0c09ffb987be42d984e7847dc15a
richardboyz <r1382333@gmail.com>
2023-10-19 21:37:05 +0800

    Set BattleCard action

--- src/components/Battle/BattleCard.jsx
+++ src/components/Battle/BattleCard.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import "./BattleCard.scss";
+import { useNavigate } from "react-router-dom";
 
 const BattleCard = ({ name, uid, isFinished }) => {
+  const navigate = useNavigate();
+
+  const toBattleField = () => {
+    if (!uid) {
+      console.log("Invlid uid!");
+      return;
+    }
+    navigate({
+      pathname: `/battle/${uid}`,
+    });
+  };
+
   return (
     <div className="battle-card">
       <span>{name}</span>
       <span style={{ color: isFinished ? "red" : "blue" }}>
         {isFinished ? "已結束" : "未結束"}
       </span>
+      {!isFinished && <button onClick={toBattleField}>進入對戰</button>}
     </div>
   );
 };
