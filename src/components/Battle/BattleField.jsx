a4c74d9a48449948ed25ac302ab0ca2b60197892
richardboyz <r1382333@gmail.com>
2023-10-19 21:38:19 +0800

    Complete BattleField layout

--- src/components/Battle/BattleField.jsx
+++ src/components/Battle/BattleField.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import "./BattleField.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHand,
+  faHandFist,
+  faHandScissors,
+} from "@fortawesome/free-solid-svg-icons";
 
 const BattleField = () => {
-  return <div>BattleField</div>;
+  const choices = [faHandFist, faHand, faHandScissors];
+
+  const { id } = useParams();
+  return (
+    <>
+      <div className="battle-field">
+        <nav className="battle-field__nav" />
+        <div className="battle-field__content"></div>
+      </div>
+      <div className="battle-field__select">
+        <button>結算</button>
+        <div className="battle-field__choices">
+          {choices.map((choice, index) => {
+            return (
+              <button className="battle-field__choice" key={index}>
+                <FontAwesomeIcon icon={choice} />
+              </button>
+            );
+          })}
+        </div>
+        <button>確定</button>
+      </div>
+    </>
+  );
 };
 
 export default BattleField;
